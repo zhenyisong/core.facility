8e49606d712d3ae923a6dbb14efb524bfedbcf0a
zhenyisong <zhenyisong@gmail.com>
2018-11-29 15:04:34 +0800

    to get the published papers, limited to China since 2018

---
+++ linuxAdmin/ncbi_pubmed.sh
@@ -0,0 +1,46 @@
+# @author Yisong Zhen
+# @since  2018-11-29
+# @reference
+# https://www.ncbi.nlm.nih.gov/books/NBK3827/#pubmedhelp.Search_Field_Descriptions_and
+# @aim
+# this PubMed query sentence is to find the first author/last author
+# papers, which all are from China
+#
+#----
+
+https://www.sciencemag.org/journals
+
+( Science Immunology[Journal] OR Science Signaling[Journal] OR
+  Science Translational Medicine[Journal] OR
+  Science Science Advances[Journal] ) AND
+  China AND 2018[DP] AND (cardiac OR heart OR cardiovascular OR vascular)
+
+search result    : 8 hits
+filtered results : 2
+
+
+https://www.cell.com/
+
+(Molecular Cell[Journal] OR Immunity[Journal] OR Developmental Cell[Journal] OR
+ Current[Journal] OR Cell Stem Cell[Journal] OR
+ Cell Reports[Journal] OR Cell Metabolism[Journal] OR Cell Host & Microbe Cell[Journal]
+ OR Chemical Biology[Journal] OR Cancer[Journal] OR Cell[Journal]) AND China AND 2018[DP]
+ AND (cardiac OR heart OR cardiovascular OR vascular)
+
+search result    : 30 hits
+filtered results : 0
+
+https://www.nature.com/siteindex
+
+( Nature Metabolism[Journal] OR  Nature Medicine[Journal] OR  Nature Genetics[Journal]
+  OR Nature Digest[Journal] OR  Nature Cell Biology[Journal] OR
+  Nature Chemical Bi[Journal]ology[jour] OR Nature[Journal] OR
+  Nature Biotechnology[Journal]
+  OR Nature Communications[Journal] OR Nature Immunology[Journal] ) AND
+  China AND 2018[DP] AND (cardiac OR heart OR cardiovascular OR vascular)
+
+
+(European Heart Journal[Jour] OR Journal of the American College of Cardiology[Jour] OR
+Circulation[Jour] OR Circulation Research[Jour]) AND China AND 2018[DP]
+search result    : 166 hits
+filtered results : 52
