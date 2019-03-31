1df864d0ee9ba89e7217490fb7940ba60345a4bd
zhenyisong <zhenyisong@gmail.com>
2019-03-31 19:39:40 +0800

    blood pressure

---
+++ expressionNGS/chen_high_pressure.R
@@ -0,0 +1,7 @@
+# This will be the new start on the blood high pressure analysis
+# to help analyze and find the clue to the charpter of gene expression pattern
+# in the patient data.
+# epigenetics and early markers and traditional medicine.
+# @author Yisong
+# @sincce 2019-03-31
+#----
